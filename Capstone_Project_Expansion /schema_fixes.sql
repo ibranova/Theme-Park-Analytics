f5cc56c8d4c2ea66f09be9110b2b44361a7fa6cb
Marcy Student <ibsory002@gmail.com>
2026-04-10 12:00:04 -0400

    Standardized attraction names and updated ride events to remove duplicated values from the database

--- Capstone_Project_Expansion /schema_fixes.sql
+++ Capstone_Project_Expansion /schema_fixes.sql
@@ -11,3 +11,34 @@ ORDER BY LOWER(attraction_name);
 
 /* we can see that there are two attractions with the same name 
 but different cases and characters: "Roller Coaster" and "roller coaster".*/
+
+
+/* Now, let's remap ride events from duplicate IDs to canonical IDs */
+
+UPDATE fact_ride_events
+SET attraction_id = 1
+WHERE attraction_id = 6;
+
+UPDATE fact_ride_events
+SET attraction_id = 2
+WHERE attraction_id = 7;
+
+SELECT COUNT(*) AS orphaned_rides_to_6
+FROM fact_ride_events WHERE attraction_id = 6;
+
+SELECT COUNT(*) AS orphaned_rides_to_7
+FROM fact_ride_events WHERE attraction_id = 7;
+
+/* Now let's remove the duplicate attractions and update the attraction names to be in a consistent format,
+ such as all title case and trimmed of any leading or trailing spaces. */
+
+DELETE FROM dim_attraction WHERE attraction_id IN (6, 7);
+
+UPDATE dim_attraction
+SET attraction_name = TRIM(attraction_name);
+
+update dim_attraction
+set attraction_name = 'Pirate Splash'
+where attraction_id = 2;
+
+
