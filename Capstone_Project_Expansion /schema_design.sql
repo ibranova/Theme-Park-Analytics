e2e8cdfafcfd26b8a0b3ed70b1f282416534bf5a
Marcy Student <ibsory002@gmail.com>
2026-04-11 18:55:14 -0400

    Created a schema design file to rewrite the database structure

---
+++ Capstone_Project_Expansion /schema_design.sql
@@ -0,0 +1,5 @@
+/* This file is for creating a proper schema desing for my database with Foreing keys 
+because there are not foreign key constraint defined in any table before.
+Instead of editing the existing table, I will write and document the DDL fron scratch directly in this file. */
+
+-- First, let's create the dim_attraction table with a primary key and appropriate data types for each column.
