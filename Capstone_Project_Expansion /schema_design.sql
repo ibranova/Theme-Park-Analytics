74f2f42c7f3747612ed556fc31f982a5a50add20
Marcy Student <ibsory002@gmail.com>
2026-04-11 19:23:02 -0400

    Recreated dimension tables with correct PK and appropriate data types to schema design

--- Capstone_Project_Expansion /schema_design.sql
+++ Capstone_Project_Expansion /schema_design.sql
@@ -2,4 +2,36 @@
 because there are not foreign key constraint defined in any table before.
 Instead of editing the existing table, I will write and document the DDL fron scratch directly in this file. */
 
--- First, let's create the dim_attraction table with a primary key and appropriate data types for each column.
+-- First, let's create all the dimension tables with their primary keys and necessary columns (No FK dependency between dimension tables):
+CREATE TABLE dim_guest (
+  guest_id              INTEGER PRIMARY KEY,
+  first_name            TEXT NOT NULL,
+  last_name             TEXT NOT NULL,
+  email                 TEXT,
+  birthdate             TEXT,
+  home_state            TEXT,
+  marketing_opt_in INTEGER CHECK (marketing_opt_in IN (0, 1))
+);
+
+CREATE TABLE dim_ticket (
+  ticket_type_id   INTEGER PRIMARY KEY,
+  ticket_type_name TEXT NOT NULL,
+  base_price_cents INTEGER NOT NULL,
+  restrictions     TEXT
+);
+
+CREATE TABLE dim_attraction (
+  attraction_id    INTEGER PRIMARY KEY,
+  attraction_name  TEXT NOT NULL UNIQUE,  -- UNIQUE prevents future duplicates
+  category         TEXT NOT NULL,
+  min_height_cm    INTEGER DEFAULT 0,
+  opened_date      TEXT
+);
+
+CREATE TABLE dim_date_ (
+  date_id    INTEGER PRIMARY KEY,
+  date_iso   TEXT NOT NULL UNIQUE,
+  day_name   TEXT NOT NULL,
+  is_weekend INTEGER NOT NULL CHECK (is_weekend IN (0, 1)),
+  season     TEXT NOT NULL
+);
